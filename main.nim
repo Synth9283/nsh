85cd1327d5cf88b44a8d645a44487a17a059b619
synth9283 <synth9283@gmail.com>
2020-07-03 16:27:50 +1000

    Implemented displaying current branch

--- main.nim
+++ main.nim
@@ -1,22 +1,18 @@
 import rdstdin, strformat, strutils, osproc, os
-import src/setup, src/getVars
+import src/setup, src/getVars, src/gitBranch
 
 # OS dependent vars
 when defined(windows):
     const RunningOn = "windows"
-    const homeDir = "USERPROFILE"
     const dirSplit = r"\"
 when defined(linux):
     const RunningOn = "linux"
-    const homeDir = "HOME"
     const dirSplit = "/"
 when defined(mac):
     const RunningOn = "macos"
-    const homeDir = "HOME"
     const dirSplit = "/"
 when defined(other):
     const RunningOn = "other"
-    const homeDir = "/"
     const dirSplit = "/"
 
 # vars
@@ -26,7 +22,7 @@ var shellFormat: string
 
 proc main() =
     while true:
-        let result: bool = readLineFromStdin(&"[{user} {getCurrentDir().split(dirSplit)[^1]}] --> ", line=line)
+        let result: bool = readLineFromStdin(&"[{user} {getCurrentDir().split(dirSplit)[^1]}{gitBranch()}] --> ", line=line)
         let command: string = line.split(" ")[0]
         let args: seq[string] = getVars(line.split(" ")[1..^1])
         case command
@@ -34,8 +30,8 @@ proc main() =
         of "cd":
             try: setCurrentDir(args[0])
             except IndexError:
-                try: setCurrentDir(getEnv(homeDir)) except OSError: discard
-            except OSError: stdout.write("The directory does not exist!")
+                try: setCurrentDir(getHomeDir()) except OSError: discard
+            except OSError: stdout.write("\nThe directory does not exist!")
         else: stdout.write(execProcess(line))
         if not result: quit(0)
 
---
+++ src/gitBranch.nim
@@ -0,0 +1,6 @@
+import osproc, os
+
+proc gitBranch*(): string =
+    if existsDir(".git"): result = " "&execProcess("git rev-parse --abbrev-ref HEAD")[0..^2]
+    else: result = ""
+    return result
