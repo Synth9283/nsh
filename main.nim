e514e2490b66d07727e24b6873f15594dcb525f3
synth9283 <synth9283@gmail.com>
2020-07-03 13:22:14 +1000

    Implemented var parsing and changing directories

--- main.nim
+++ main.nim
@@ -1,15 +1,19 @@
 import rdstdin, strformat, strutils, osproc, os
-import src/setup
+import src/setup, src/getVars
 
 # check the os
 when defined(windows):
     const RunningOn = "windows"
+    const homeDir = "USERPROFILE"
 when defined(linux):
     const RunningOn = "linux"
+    const homeDir = "HOME"
 when defined(mac):
     const RunningOn = "macos"
+    const homeDir = "HOME"
 when defined(other):
     const RunningOn = "other"
+    const homeDir = "/"
 
 # vars
 var line: string
@@ -20,10 +24,15 @@ proc main() =
     while true:
         let result: bool = readLineFromStdin(&"[{user}] --> ", line=line)
         let command: string = line.split(" ")[0]
-        let args: seq[string] = line.split(" ")[1..^1]
+        let args: seq[string] = getVars(line.split(" ")[1..^1])
         case command
         of "exit": quit(0)
-        of "cd": setCurrentDir(args[0])
+        of "cd":
+            try: setCurrentDir(args[0])
+            except IndexError:
+                try: setCurrentDir(getEnv(homeDir))
+                except OSError: echo getEnv(homeDir)
+            except OSError: echo "The directory does not exist!"
         else: echo execProcess(line)
         if not result: quit(0)
 
---
+++ src/getVars.nim
@@ -0,0 +1,8 @@
+import os
+
+proc getVars*(args: seq[string]): seq[string] =
+    result = @[]
+    for arg in args:
+        if arg[0] == '$': result.add(getEnv(arg[1..^1]))
+        else: result.add(arg)
+    return result
