ae64a5695342eaade550ae720e1d4c60a01ccf77
synth9283 <synth9283@gmail.com>
2020-07-04 12:24:46 +1000

    main.nim shell promt color update

--- main.nim
+++ main.nim
@@ -3,26 +3,44 @@ import src/setup, src/getVars, src/gitBranch
 
 # OS dependent vars
 when defined(windows):
-    const RunningOn = "windows"
-    const dirSplit = r"\"
+    const
+        RunningOn: string = "windows"
+        dirSplit: string = r"\"
 when defined(linux):
-    const RunningOn = "linux"
-    const dirSplit = "/"
+    const
+        RunningOn: string = "linux"
+        dirSplit: string = "/"
 when defined(mac):
-    const RunningOn = "macos"
-    const dirSplit = "/"
+    const
+        RunningOn: string = "macos"
+        dirSplit: string = "/"
 when defined(other):
-    const RunningOn = "other"
-    const dirSplit = "/"
+    const
+        RunningOn: string = "other"
+        dirSplit: string = "/"
 
-# vars
-var line: string
-var user: string
-var shellFormat: string
+# shell vars
+var
+    line: string
+    user: string
+    shellFormat: string
+
+# ansi vars
+const
+    black: string = "\x1b[30m"
+    red: string = "\x1b[31m"
+    green: string = "\x1b[32m"
+    yellow: string = "\x1b[33m"
+    blue: string = "\x1b[34m"
+    megenta: string = "\x1b[35m"
+    cyan: string = "\x1b[36m"
+    white: string = "\x1b[37m"
+    reset: string = "\x1b[0m"
+    clear: string = "\x1b[2J"
 
 proc main() =
     while true:
-        let result: bool = readLineFromStdin(&"[{user} {getCurrentDir().split(dirSplit)[^1]}{gitBranch()}] --> ", line=line)
+        let result: bool = readLineFromStdin(&"[{blue}{user} {green}{getCurrentDir().split(dirSplit)[^1]}{cyan}{gitBranch()}{reset}] {megenta}--> {reset}", line=line)
         let command: string = line.split(" ")[0]
         let args: seq[string] = getVars(line.split(" ")[1..^1])
         case command
@@ -32,6 +50,8 @@ proc main() =
             except IndexError:
                 try: setCurrentDir(getHomeDir()) except OSError: discard
             except OSError: stdout.write("\nThe directory does not exist!")
+        # of "clear":
+        #     echo clear
         else: stdout.write(execProcess(line))
         if not result: quit(0)
 
