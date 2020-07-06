8af852404d47313725684325bdbf5a44646d9c5f
codic <codic@linuxmail.org>
2020-07-05 17:25:37 -0700

    Implement help pages

--- src/runLine.nim
+++ src/runLine.nim
@@ -1,4 +1,12 @@
-import os, strutils, osproc
+import os, strutils, osproc, tables
+proc help(item:string):string = 
+    const descriptions = {
+        "cd": "Change the directory. Syntax: `cd directory_path`.",
+        "exit": "Quit the shell with an exit status of 0. Syntax: `exit`. Custom exit codes will be implemented in the future.",
+        "export": "Export environment variables (not shell variables). Syntax: `export VARIABLE_NAME=value` (You may use no quotes, single quotes, or double quotes)."
+    }.toTable
+    if item in descriptions: return descriptions[item]
+    else: return ""
 proc runLine*(command:string, line:string, args:seq[string]):string = 
     case command
     of "exit": quit(0)
@@ -20,4 +28,8 @@ proc runLine*(command:string, line:string, args:seq[string]):string =
             for line in readFile(script).split("\n"):
                 let command: string = line.split(" ")[0]
                 stdout.write(runLine(command, line, args))
+    of "help", "man":
+       var hp = help(args.join(" "))
+       if hp == "": echo "No help page found for " & args.join(" ")
+       else: echo hp
     else: stdout.write(execProcess(line)); stdout.flushFile()
