6e4660296435786ecb5d06698f4a500a9388cbe6
codic <codic@linuxmail.org>
2020-07-14 17:54:15 -0700

    revert: linenoise has same problems as rdstdin as rdstdin is just wrapper for linenoise

--- main.nim
+++ main.nim
@@ -1,5 +1,5 @@
 import strformat, strutils, os
-import src/setup, src/getArgs, src/colorsAnsi, src/homeDir, src/gitBranch, src/runLine, linenoise
+import src/setup, src/getArgs, src/colorsAnsi, src/homeDir, src/gitBranch, src/runLine, src/customReadline
 
 # OS dependent variables for Windows, MacOS, Linux, and other operating systems (assumed to be UNIX comliant)
 when defined(windows):
@@ -21,33 +21,29 @@ when defined(other):
 
 # shell vars
 var
+    line: string
     user: string
     shellFormat: string
 
-let cnsh = getHomeDir() & ".nshrc"
-let history = getHomeDir() & ".nsh_history"
-
 proc main() =
-
+    let cnsh = getHomeDir() & ".nshrc"
     if fileExists(cnsh): 
         for line in readFile(cnsh).split("\n"):
             let command: string = line.split(" ")[0]
             let args: seq[string] = getArgs(line.split(" ")[1..^1])
             runLine(command, line, args)
-    if fileExists(history):
-        historyAdd(history.cstring)
     while true:
         var promptchar: string
         if existsEnv("PROMPTCHAR"): promptchar = getEnv("PROMPTCHAR")
         else: promptchar = ">"
-        let linec = linenoise.readLine(&"{blue}{homeDir(getCurrentDir())}{green}{gitBranch()}{magenta} {promptchar} {resetc}")
-        let line = $linec
-        historyAdd(linec)
+        stdout.write(&"{blue}{homeDir(getCurrentDir())}{green}{gitBranch()}{magenta} {promptchar} {resetc}")
+        stdout.flushFile()
+        let result: bool = stdin.customReadLine(line)
         let command: string = line.split(" ")[0]
         let args: seq[string] = getArgs(line.split(" ")[1..^1])
-        runLine(command, line, args)
+        if not line.endsWith("^C"): runLine(command, line, args)
+        elif not result: quit(0)
 
 when isMainModule:
     setup(RunningOn, user, shellFormat)
     main()
-    discard historySave(history)
